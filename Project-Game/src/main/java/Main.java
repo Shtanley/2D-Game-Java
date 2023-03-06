8f77b54ec3afed5b842cf7274c800c1af7b4a460
Michael Plunkett <mic.a.plunkett@gmail.com>
2023-03-05 17:20:29 -0800

    moved pom.xml

--- Project-Game/src/main/java/Main.java
+++ Project-Game/src/main/java/Main.java
@@ -1,8 +1,12 @@
 package main;
+import GameMap;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Game myGame = new Game();
+
     }
 }
