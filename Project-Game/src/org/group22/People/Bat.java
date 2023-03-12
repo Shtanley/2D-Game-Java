3a6f8e1e7d5f110d10144e2604de58b1c1523f8f
Michael Plunkett <mic.a.plunkett@gmail.com>
2023-03-12 15:17:39 -0700

    Removed comment

--- Project-Game/src/org/group22/People/Bat.java
+++ Project-Game/src/org/group22/People/Bat.java
@@ -1,8 +1,6 @@
 package org.group22.People;
 
 public class Bat extends Enemy{
-    //todo someone figure out iterator works
-    //Iterator iterator = new It
 
     public Bat() {
         super();
