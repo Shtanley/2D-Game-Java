38c2eee7196495c3398caacdcbb3cfccd455fc87
dhz1 <dina_zeng@sfu.ca>
2023-03-30 06:53:51 -0700

    fixed

--- Project-Game/src/org/group22/Drops/BonusReward.java
+++ Project-Game/src/org/group22/Drops/BonusReward.java
@@ -7,18 +7,14 @@ package org.group22.Drops;
  * @author Michael
  */
 public abstract class BonusReward extends Item{
-    private final int healthAdjustment = 50;
-    private final int pointAdjustment = 50;
-    private static final int spawnTimer = 100;
-    private static final double spawnChance = 0.3;
     public long birthTime;
     public long lifetime;
 
-    public int getHealthAdjustment() { return healthAdjustment;}
+    public int getHealthAdjustment() { return 50;}
     public int getPointAdjustment() {
-        return pointAdjustment;
+        return 50;
     }
 
-    public static int getSpawnTimer() { return spawnTimer; }
-    public static double getSpawnChance() {return spawnChance;}
+    public static int getSpawnTimer() { return 100; }
+    public static double getSpawnChance() {return 0.3;}
 }
