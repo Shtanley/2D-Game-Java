6199b4051f3237d6440ac71bfeb42a01d7e71f66
dhz1 <dina_zeng@sfu.ca>
2023-04-01 20:47:11 -0700

    created test directory

---
+++ Project-Game/src/test/CharacterCreation.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.group22.People.Player;
+import org.testng.annotations.Test;
+
+public class CharacterCreation {
+
+    @Test
+    public void testPlayerCreation(){
+       //test here
+    }
+}
