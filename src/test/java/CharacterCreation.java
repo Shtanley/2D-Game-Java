c9b6101af0ac0718cc43f19082fb370fdacd9017
Michael Plunkett <mic.a.plunkett@gmail.com>
2023-04-03 20:24:51 -0700

    Many changes to file structure and pom, still not working

--- src/test/java/CharacterCreation.java
+++ src/test/java/CharacterCreation.java
@@ -2,14 +2,13 @@ package test.java;
 
 import main.java.People.Player;
 import main.java.app.GamePanel;
-import org.testng.annotations.Test;
 
 public class CharacterCreation {
     private final GamePanel testGP = new GamePanel();
     private final Player testPlayer = new Player(testGP, testGP.getKeyInputs());
 
-    @Test
-    public void testPlayerCreation(){
-
-    }
+//    @Test
+//    public void testPlayerCreation(){
+//
+//    }
 }
