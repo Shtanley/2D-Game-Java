7a436eb4569b7281999cc292f7dcbd21b2c03501
Michael Plunkett <mic.a.plunkett@gmail.com>
2023-04-04 22:32:22 -0700

    Made unit tests for enemy enemy collision

--- src/test/java/integration/CharacterCharacterCollisionTest.java
+++ src/test/java/integration/CharacterCharacterCollisionTest.java
@@ -1,8 +1,6 @@
 package integration;
 
-import People.Bat;
-import People.Enemy;
-import People.Player;
+import People.*;
 import app.CollisionChecker;
 import app.GamePanel;
 import app.GameSettings;
@@ -29,32 +27,35 @@ public class CharacterCharacterCollisionTest {
 
         int index = cCheck.checkEntity(player, gamePanel.enemies);
         Assert.assertEquals(0, index);
+        Assert.assertFalse(player.isCollisionOff());
     }
 
     @Test
     public void PlayerEnemyCollisionTest2(){
         player.setPlayerValues(5, 5, 0, "down");
-        Bat bat = new Bat(gamePanel);
-        bat.setWorldX(5 * GameSettings.getTileSize());
-        bat.setWorldY(10 * GameSettings.getTileSize());
-        bat.setSpeed(0);
-        gamePanel.enemies = new Enemy[]{bat};
+        Skeleton skeleton = new Skeleton(gamePanel);
+        skeleton.setWorldX(5 * GameSettings.getTileSize());
+        skeleton.setWorldY(10 * GameSettings.getTileSize());
+        skeleton.setSpeed(0);
+        gamePanel.enemies = new Enemy[]{skeleton};
 
         int index = cCheck.checkEntity(player, gamePanel.enemies);
         Assert.assertEquals(-1, index);
+        Assert.assertTrue(player.isCollisionOff());
     }
 
     @Test
     public void PlayerEnemyCollisionTest3(){
         player.setPlayerValues(5, 5, 0, "down");
-        Bat bat = new Bat(gamePanel);
-        bat.setWorldX(10 * GameSettings.getTileSize());
-        bat.setWorldY(5 * GameSettings.getTileSize());
-        bat.setSpeed(0);
-        gamePanel.enemies = new Enemy[]{bat};
+        Slime slime = new Slime(gamePanel);
+        slime.setWorldX(10 * GameSettings.getTileSize());
+        slime.setWorldY(5 * GameSettings.getTileSize());
+        slime.setSpeed(0);
+        gamePanel.enemies = new Enemy[]{slime};
 
         int index = cCheck.checkEntity(player, gamePanel.enemies);
         Assert.assertEquals(-1, index);
+        Assert.assertTrue(player.isCollisionOff());
     }
 
     @Test
@@ -68,5 +69,47 @@ public class CharacterCharacterCollisionTest {
 
         int index = cCheck.checkEntity(player, gamePanel.enemies);
         Assert.assertEquals(-1, index);
+        Assert.assertTrue(player.isCollisionOff());
+    }
+
+    @Test
+    public void EnemyEnemyCollisionTest1(){
+        Skeleton skeleton = new Skeleton(gamePanel);
+        skeleton.setWorldX(10 * GameSettings.getTileSize());
+        skeleton.setWorldY(10 * GameSettings.getTileSize());
+        skeleton.setSpeed(0);
+        Slime slime = new Slime(gamePanel);
+        slime.setWorldX(10 * GameSettings.getTileSize());
+        slime.setWorldY(10 * GameSettings.getTileSize());
+        slime.setSpeed(0);
+        gamePanel.enemies = new Enemy[]{skeleton, slime};
+
+        int index = cCheck.checkEntity(skeleton, gamePanel.enemies);
+        Assert.assertEquals(1, index);
+        Assert.assertFalse(skeleton.isCollisionOff());
+
+        index = cCheck.checkEntity(slime, gamePanel.enemies);
+        Assert.assertEquals(0, index);
+        Assert.assertFalse(slime.isCollisionOff());
+    }
+    @Test
+    public void EnemyEnemyCollisionTest2(){
+        Bat bat1 = new Bat(gamePanel);
+        bat1.setWorldX(5 * GameSettings.getTileSize());
+        bat1.setWorldY(5 * GameSettings.getTileSize());
+        bat1.setSpeed(0);
+        Bat bat2 = new Bat(gamePanel);
+        bat2.setWorldX(10 * GameSettings.getTileSize());
+        bat2.setWorldY(10 * GameSettings.getTileSize());
+        bat2.setSpeed(0);
+        gamePanel.enemies = new Enemy[]{bat1, bat2};
+
+        int index = cCheck.checkEntity(bat1, gamePanel.enemies);
+        Assert.assertEquals(-1, index);
+        Assert.assertTrue(bat1.isCollisionOff());
+
+        index = cCheck.checkEntity(bat2, gamePanel.enemies);
+        Assert.assertEquals(-1, index);
+        Assert.assertTrue(bat2.isCollisionOff());
     }
 }
